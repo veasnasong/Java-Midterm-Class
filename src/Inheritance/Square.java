dbe4b3eab38c0484a6319c36dc69325f7e84fb8a
Song Veasna <156062281+veasnasong@users.noreply.github.com>
2024-01-09 21:51:44 -0400

    Update Square.java

--- src/Inheritance/Square.java
+++ src/Inheritance/Square.java
@@ -1,2 +1,8 @@
-package Inheritance;public class Square {
+package Inheritance;
+public class Square {
+  / Constructor for Square
+    public Square(double side) {
+        super(side, side);
+        System.out.println("Super is:"+ side);
+    }
 }
