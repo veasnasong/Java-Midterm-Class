d182ebc5584580dd4c83f89ff6d184c9834932bb
Song Veasna <156062281+veasnasong@users.noreply.github.com>
2024-01-09 21:51:06 -0400

    Update Rectangle.java

--- src/Inheritance/Rectangle.java
+++ src/Inheritance/Rectangle.java
@@ -1,2 +1,18 @@
-package Inheritance;public class Rectangle {
+package Inheritance;
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
 }
