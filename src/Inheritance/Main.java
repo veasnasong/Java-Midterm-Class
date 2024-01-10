07ba36d6e9f9e1476e266b1cd09af59a95897c58
Song Veasna <156062281+veasnasong@users.noreply.github.com>
2024-01-09 21:52:55 -0400

    Update Main.java

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            // Creating a Rectangle
+            Rectangle rectangle = new Rectangle(5, 8);
+            System.out.println("Rectangle Area: " + rectangle.getArea());
+            System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+
+            // Creating a Square
+            Square square = new Square(4);
+            System.out.println("Square Area: " + square.getArea());
+            System.out.println("Square Perimeter: " + square.getPerimeter());
+    }
+        
     }
 }
